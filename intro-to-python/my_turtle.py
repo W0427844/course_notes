9e8efdb3a984f114bebcb6feb99fedf3a964bcd2
Wiktoria Biernat <W0427844@nscc.ca>
2019-09-16 10:08:45 -0300

    python try 2

---
+++ intro-to-python/my_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.color("yellow")
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(140)
+
+turtle.color("red")
+turtle.right(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(150)
